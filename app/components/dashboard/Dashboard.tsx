0ce10146ed7b7ec9ba19a0bfb3370683b502e22e
klaudijus <klaudijus.strategeens@gmail.com>
2021-12-20 19:44:21 +0700

    Create mock dashboard component

---
+++ app/components/dashboard/Dashboard.tsx
@@ -0,0 +1,155 @@
+import React, {useState} from 'react';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import {useSelector} from 'react-redux';
+import {
+    PrimaryHeader,
+    PrimaryText,
+    SecondaryHeader,
+    SecondaryText
+} from '../ui/Text';
+import theme from '../../theme';
+import {Activity} from '../../types/Activity';
+import utils from '../../styles-utilities';
+import SelectionButtons from '../shared/SelectionButtons';
+import EntryEffortBar from '../entry/EntryEffortBar';
+import {getEntries} from '../../redux/slices/entriesSlice';
+import {getEntriesFilter} from '../../redux/slices/entriesFilterSlice';
+
+const items = [
+    {
+        label: 'Week',
+        value: 1
+    },
+    {
+        label: 'Month',
+        value: 2
+    },
+    {
+        label: 'Year',
+        value: 3
+    },
+    {
+        label: 'All',
+        value: 4
+    }
+];
+
+const getActivityFilterText = (filter: Activity | null) => {
+    if (filter === Activity.RUNNING) return 'Running';
+    if (filter === Activity.CYCLING) return 'Cycling';
+    if (filter === Activity.SWIMMING) return 'Swimming';
+    return 'All Activity';
+};
+
+function Dashboard() {
+    const filter = useSelector(getEntriesFilter);
+    const entries = useSelector(getEntries);
+    const [timeInterval, setTimeInterval] = useState(1);
+    return (
+        <View style={styles.panel}>
+            <SelectionButtons
+                selected={timeInterval}
+                items={items}
+                style={[utils.marginBottomXL]}
+                onChange={setTimeInterval}
+            />
+            <View style={[utils.marginBottomXL]}>
+                <PrimaryText style={styles.dateText}>
+                    December, 2021
+                </PrimaryText>
+                <PrimaryHeader>
+                    {getActivityFilterText(filter).toUpperCase()}
+                </PrimaryHeader>
+            </View>
+            <View
+                style={[utils.row, utils.justifyBetween, utils.marginBottomL]}
+            >
+                <View>
+                    <SecondaryHeader style={styles.labelledText}>
+                        120km
+                    </SecondaryHeader>
+                    <SecondaryText>Distance</SecondaryText>
+                </View>
+                <View>
+                    <SecondaryHeader style={styles.labelledText}>
+                        17h 15min
+                    </SecondaryHeader>
+                    <SecondaryText>Duration</SecondaryText>
+                </View>
+            </View>
+            <View style={[utils.row]}>
+                <View style={styles.secondaryDetails}>
+                    <PrimaryText style={styles.secondaryDetailsText}>
+                        15
+                    </PrimaryText>
+                    <SecondaryText>Workouts</SecondaryText>
+                </View>
+                <View style={styles.secondaryDetails}>
+                    <PrimaryText style={styles.secondaryDetailsText}>
+                        5&apos;48&quot;
+                    </PrimaryText>
+                    <SecondaryText>Avg. Pace</SecondaryText>
+                </View>
+            </View>
+            <View style={styles.separator} />
+            <View>
+                <View
+                    style={[
+                        utils.row,
+                        utils.alignEnd,
+                        utils.justifyBetween,
+                        utils.marginBottomS
+                    ]}
+                >
+                    <SecondaryText>Avg. Intensity</SecondaryText>
+                    <PrimaryText style={styles.secondaryDetailsText}>
+                        3/5
+                    </PrimaryText>
+                </View>
+                <EntryEffortBar effort={4} />
+            </View>
+            <View style={styles.backgroundSlice} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    panel: {
+        paddingRight: theme.SPACING.XL,
+        paddingBottom: 104,
+        paddingLeft: theme.SPACING.XL,
+        marginBottom: theme.SPACING.M,
+        backgroundColor: theme.COLORS.BACKGROUND_PRIMARY
+    },
+    dateText: {
+        marginBottom: theme.SPACING.XS,
+        fontSize: theme.FONT_SIZE.SECONDARY
+    },
+    labelledText: {
+        marginBottom: theme.SPACING.XS
+    },
+    secondaryDetails: {
+        marginRight: theme.SPACING.XL
+    },
+    secondaryDetailsText: {
+        fontFamily: 'LatoBlack',
+        fontSize: 24,
+        paddingBottom: theme.SPACING.XS
+    },
+    backgroundSlice: {
+        position: 'absolute',
+        bottom: 0,
+        borderRightWidth: Dimensions.get('window').width,
+        borderBottomWidth: 70,
+        borderBottomColor: theme.COLORS.BACKGROUND_SECONDARY,
+        borderColor: 'transparent'
+    },
+    separator: {
+        backgroundColor: theme.COLORS.BACKGROUND_TERTIARY,
+        marginTop: theme.SPACING.L,
+        marginBottom: theme.SPACING.L,
+        height: 2
+    }
+});
+
+export default Dashboard;
--- app/screens/EntryListScreen.tsx
+++ app/screens/EntryListScreen.tsx
@@ -1,18 +1,25 @@
 import React, {useLayoutEffect} from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import appStyles from '../styles';
 import HeaderActivityFilter from '../components/header/HeaderActivityFilter';
 import EntryList from '../components/entry/EntryList';
 import TrophyCongratulations from '../components/trophy/TrophyCongratulations';
 import HeaderButton from '../components/header/HeaderButton';
+import Dashboard from '../components/dashboard/Dashboard';
+import theme from '../theme';
 
 function EntryListScreen({navigation}) {
     const navigateToEntryForm = () => navigation.navigate('entry-form', {});
-    const navigateToEntryDetails = (id) =>
+    const navigateToEntryDetails = id =>
         navigation.navigate('entry-details', {id});
 
     useLayoutEffect(() => {
         navigation.setOptions({
+            headerStyle: {
+                height: theme.HEADER_HEIGHT,
+                backgroundColor: theme.COLORS.BACKGROUND_PRIMARY,
+                elevation: 0
+            },
             headerLeft: null,
             headerRight: () => (
                 <HeaderButton iconName="plus" onPress={navigateToEntryForm} />
@@ -24,6 +31,7 @@ function EntryListScreen({navigation}) {
         <>
             <HeaderActivityFilter />
             <View style={appStyles.screenContainer}>
+                <Dashboard />
                 <EntryList onPress={navigateToEntryDetails} />
                 <TrophyCongratulations />
             </View>
