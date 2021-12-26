3e8ccafef3ab4540dad0f62fb4d40e8a873042dd
klaudijus <klaudijus.strategeens@gmail.com>
2021-12-26 14:06:26 +0700

    Add entries functions to api

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,27 +1,27 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Entry} from '../types/Entry';
+import Storage from './storage';
 
 const FILTERS_KEY = 'filters';
+const ENTRIES_KEY = 'entries';
 
-function logError(error: unknown) {
-    // eslint-disable-next-line no-console
-    console.log('API Storage error', error);
+export async function fetchFilters(): Promise<object> {
+    const filter = await Storage.getItem<object>(FILTERS_KEY);
+    return filter || {};
 }
 
 export async function saveFilters(filters) {
-    try {
-        return AsyncStorage.setItem(FILTERS_KEY, JSON.stringify(filters));
-    } catch (e) {
-        logError(e);
-        return Promise.reject();
-    }
+    return Storage.setItem(FILTERS_KEY, filters);
 }
 
-export async function fetchFilters() {
-    try {
-        const filters = await AsyncStorage.getItem(FILTERS_KEY);
-        return filters ? JSON.parse(filters) : {};
-    } catch (e) {
-        logError(e);
-        return Promise.reject();
-    }
+export async function fetchEntries(): Promise<Entry[]> {
+    const entries = await Storage.getItem<Entry[]>(ENTRIES_KEY);
+    return entries || [];
+}
+
+export async function saveEntry(entry: Entry) {
+    return Storage.upsertToArray<Entry>(ENTRIES_KEY, entry);
+}
+
+export async function deleteEntry(id: string) {
+    return Storage.deleteFromArray<Entry>(ENTRIES_KEY, id);
 }
