f73792d180bc0b32460647bf7d635dfb4ba1de46
klaudijus <klaudijus.strategeens@gmail.com>
2022-01-08 13:37:46 +0700

    Create minimal line chart component

---
+++ app/components/chart/MinimalLineChart.tsx
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as shape from 'd3-shape';
+import React from 'react';
+import {View, ViewStyle} from 'react-native';
+import {LineChart} from 'react-native-svg-charts';
+import theme from '../../theme';
+import utils from '../../styles-utilities';
+
+interface MinimalLineChartProps {
+    data: number[];
+    style?: ViewStyle | ViewStyle[];
+}
+
+function MinimalLineChart({data, style}: MinimalLineChartProps) {
+    return (
+        <View style={[utils.flex1, style]}>
+            <LineChart
+                style={{flex: 1}}
+                data={data}
+                svg={{strokeWidth: 2, stroke: theme.COLORS.THEME_SECONDARY}}
+                curve={shape.curveNatural}
+                contentInset={{top: 5, bottom: 5}}
+            />
+        </View>
+    );
+}
+
+export default MinimalLineChart;
