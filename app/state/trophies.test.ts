1d5014b8b17cb813ffdfe52d2368437b8749b5f5
klaudijus <klaudijus.strategeens@gmail.com>
2022-01-17 09:22:13 +0700

    Add individual trophies completion logic

--- app/state/trophies.test.ts
+++ app/state/trophies.test.ts
@@ -5,11 +5,14 @@ import {
     deleteTrophy,
     getTrophies,
     loadTrophies,
+    updateCompletedTrophies,
     updateTrophy,
     useTrophiesStore
 } from './trophies';
 import * as api from '../lib/api';
 import {Trophy, TrophyType} from '../types/Trophy';
+import {useEntriesStore} from './entries';
+import {Entry} from '../types/Entry';
 
 jest.mock('../lib/storage');
 jest.mock('../../assets/data/trophies.json', () => []);
@@ -42,7 +45,6 @@ describe('Trophies state', () => {
         beforeEach(() => {
             saveTrophySpy = jest.spyOn(api, 'saveTrophy');
             deleteTrophySpy = jest.spyOn(api, 'deleteTrophy');
-            useTrophiesStore.setState(initialState);
         });
 
         it('should load trophies', async () => {
@@ -55,6 +57,7 @@ describe('Trophies state', () => {
         });
 
         it('should add trophy', () => {
+            useTrophiesStore.setState(initialState);
             const trophy: Trophy = {id: '2'} as Trophy;
             addTrophy(trophy);
             expect(useTrophiesStore.getState().trophies).toEqual([
@@ -65,6 +68,7 @@ describe('Trophies state', () => {
         });
 
         it('should update trophy', () => {
+            useTrophiesStore.setState(initialState);
             const trophy: Trophy = {
                 id: '1',
                 activity: Activity.SWIMMING
@@ -77,9 +81,241 @@ describe('Trophies state', () => {
         });
 
         it('should delete trophy', () => {
+            useTrophiesStore.setState(initialState);
             deleteTrophy('1');
             expect(useTrophiesStore.getState().trophies).toEqual([]);
             expect(deleteTrophySpy).toHaveBeenCalled();
+        });
+    });
+
+    describe('completed trophies', () => {
+        beforeEach(() => {
+            useTrophiesStore.setState({
+                trophies: [
+                    {
+                        id: '1',
+                        activity: Activity.RUNNING,
+                        type: TrophyType.TOTAL,
+                        entryId: null,
+                        distance: 0,
+                        duration: 7200,
+                        completedAt: null,
+                        completed: false
+                    },
+                    {
+                        id: '2',
+                        activity: Activity.RUNNING,
+                        type: TrophyType.TOTAL,
+                        entryId: null,
+                        distance: 20,
+                        duration: 0,
+                        completedAt: null,
+                        completed: false
+                    },
+                    {
+                        id: '3',
+                        activity: Activity.RUNNING,
+                        type: TrophyType.INDIVIDUAL,
+                        entryId: null,
+                        distance: 15,
+                        duration: 0,
+                        completedAt: null,
+                        completed: false
+                    },
+                    {
+                        id: '4',
+                        activity: Activity.RUNNING,
+                        type: TrophyType.INDIVIDUAL,
+                        entryId: null,
+                        distance: 0,
+                        duration: 5000,
+                        completedAt: null,
+                        completed: false
+                    },
+                    {
+                        id: '5',
+                        activity: Activity.RUNNING,
+                        type: TrophyType.INDIVIDUAL,
+                        entryId: null,
+                        distance: 11,
+                        duration: 3600,
+                        completedAt: null,
+                        completed: false
+                    }
+                ] as Trophy[]
+            });
+        });
+
+        it('should update with individual distance', () => {
+            useEntriesStore.setState({
+                entries: [
+                    {
+                        id: '999',
+                        date: '2022-01-16',
+                        activity: Activity.RUNNING,
+                        distance: 16,
+                        duration: 0
+                    },
+                    {
+                        id: '1000',
+                        date: '2022-01-17',
+                        activity: Activity.SWIMMING,
+                        distance: 20,
+                        duration: 0
+                    }
+                ] as Entry[]
+            });
+            updateCompletedTrophies();
+            expect(
+                useTrophiesStore
+                    .getState()
+                    .trophies.filter((trophy) => trophy.completed)
+            ).toEqual([
+                {
+                    id: '3',
+                    activity: Activity.RUNNING,
+                    type: TrophyType.INDIVIDUAL,
+                    entryId: '999',
+                    distance: 15,
+                    duration: 0,
+                    completedAt: '2022-01-16',
+                    completed: true
+                },
+                {
+                    id: '5',
+                    activity: Activity.RUNNING,
+                    type: TrophyType.INDIVIDUAL,
+                    entryId: '999',
+                    distance: 11,
+                    duration: 3600,
+                    completedAt: '2022-01-16',
+                    completed: true
+                }
+            ]);
+        });
+
+        it('should update with individual duration', () => {
+            useEntriesStore.setState({
+                entries: [
+                    {
+                        id: '999',
+                        date: '2022-01-16',
+                        activity: Activity.RUNNING,
+                        distance: 0,
+                        duration: 5000
+                    }
+                ] as Entry[]
+            });
+            updateCompletedTrophies();
+            expect(
+                useTrophiesStore
+                    .getState()
+                    .trophies.filter((trophy) => trophy.completed)
+            ).toEqual([
+                {
+                    id: '4',
+                    activity: Activity.RUNNING,
+                    type: TrophyType.INDIVIDUAL,
+                    entryId: '999',
+                    distance: 0,
+                    duration: 5000,
+                    completedAt: '2022-01-16',
+                    completed: true
+                }
+            ]);
+        });
+
+        it('should update with individual pace', () => {
+            useEntriesStore.setState({
+                entries: [
+                    {
+                        id: '999',
+                        date: '2022-01-16',
+                        activity: Activity.RUNNING,
+                        distance: 12,
+                        duration: 3500
+                    }
+                ] as Entry[]
+            });
+
+            updateCompletedTrophies();
+            expect(
+                useTrophiesStore
+                    .getState()
+                    .trophies.filter((trophy) => trophy.completed)
+            ).toEqual([
+                {
+                    id: '5',
+                    activity: Activity.RUNNING,
+                    type: TrophyType.INDIVIDUAL,
+                    entryId: '999',
+                    distance: 11,
+                    duration: 3600,
+                    completedAt: '2022-01-16',
+                    completed: true
+                }
+            ]);
+        });
+
+        it('should reset when no conditions met', () => {
+            useTrophiesStore.setState({
+                trophies: [
+                    {
+                        id: '1',
+                        activity: Activity.RUNNING,
+                        type: TrophyType.INDIVIDUAL,
+                        entryId: '999',
+                        distance: 20,
+                        duration: 7200,
+                        completedAt: '2022-01-16',
+                        completed: true
+                    },
+                    {
+                        id: '2',
+                        activity: Activity.SWIMMING,
+                        type: TrophyType.INDIVIDUAL,
+                        entryId: '1000',
+                        distance: 5,
+                        duration: 0,
+                        completedAt: '2022-01-16',
+                        completed: true
+                    }
+                ] as Trophy[]
+            });
+            useEntriesStore.setState({
+                entries: [
+                    {
+                        id: '999',
+                        date: '2022-01-16',
+                        activity: Activity.RUNNING,
+                        distance: 19,
+                        duration: 0
+                    }
+                ] as Entry[]
+            });
+            updateCompletedTrophies();
+            expect(useTrophiesStore.getState().trophies).toEqual([
+                {
+                    id: '1',
+                    activity: Activity.RUNNING,
+                    type: TrophyType.INDIVIDUAL,
+                    entryId: null,
+                    distance: 20,
+                    duration: 7200,
+                    completedAt: null,
+                    completed: false
+                },
+                {
+                    id: '2',
+                    activity: Activity.SWIMMING,
+                    type: TrophyType.INDIVIDUAL,
+                    entryId: null,
+                    distance: 5,
+                    duration: 0,
+                    completedAt: null,
+                    completed: false
+                }
+            ]);
         });
     });
 
--- app/state/trophies.ts
+++ app/state/trophies.ts
@@ -1,10 +1,13 @@
 import create from 'zustand';
-import {Trophy} from '../types/Trophy';
+import {Trophy, TrophySubtype, TrophyType} from '../types/Trophy';
 import * as api from '../lib/api';
 // eslint-disable-next-line import/extensions
 import trophiesJson from '../../assets/data/trophies.json';
 import {Activity} from '../types/Activity';
 import {generateId} from '../lib/utility';
+import {useEntriesStore} from './entries';
+import {getTrophySubtype} from '../lib/trophy';
+import {Entry} from '../types/Entry';
 
 const predefinedTrophies = trophiesJson as Trophy[];
 
@@ -48,6 +51,70 @@ export async function loadTrophies() {
     useTrophiesStore.setState(() => ({
         trophies: [...predefinedTrophies, ...trophies]
     }));
+}
+
+function isTrophyCompleted(
+    trophy: Trophy,
+    entries: Entry[]
+): Entry | undefined {
+    const subtype = getTrophySubtype(trophy);
+    return entries
+        .filter((entry) => entry.activity === trophy.activity)
+        .find((entry) => {
+            const isDistanceCompleted =
+                subtype === TrophySubtype.DISTANCE &&
+                entry.distance >= trophy.distance;
+
+            const isDurationCompleted =
+                subtype === TrophySubtype.DURATION &&
+                entry.duration >= trophy.duration;
+
+            const isPaceCompleted =
+                subtype === TrophySubtype.PACE &&
+                entry.distance >= trophy.distance &&
+                entry.duration <= trophy.duration;
+
+            return (
+                isDistanceCompleted || isDurationCompleted || isPaceCompleted
+            );
+        });
+}
+
+export function updateCompletedTrophies() {
+    const {entries} = useEntriesStore.getState();
+    const {trophies} = useTrophiesStore.getState();
+
+    const entriesMap = entries.reduce((map, entry) => {
+        map.set(entry.id, entry);
+        return map;
+    }, new Map<string, Entry>());
+
+    const individualTrophies = trophies.filter(
+        (trophy) => trophy.type === TrophyType.INDIVIDUAL
+    );
+
+    individualTrophies.forEach((trophy) => {
+        const entry = isTrophyCompleted(trophy, entries);
+        updateTrophy({
+            ...trophy,
+            completed: !!entry,
+            completedAt: entry ? entry.date : null,
+            entryId: entry ? entry.id : null
+        });
+    });
+
+    individualTrophies
+        .filter((trophy) => trophy.completed)
+        .forEach((trophy) => {
+            if (!entriesMap.has(trophy.entryId as string)) {
+                updateTrophy({
+                    ...trophy,
+                    completed: false,
+                    completedAt: null,
+                    entryId: null
+                });
+            }
+        });
 }
 
 export const getTrophy = (state: TrophiesState, id: string) =>
