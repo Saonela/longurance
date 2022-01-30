d5b4fe9be984eaeea8ddc97a58b4f73b746c432f
klaudijus <klaudijus.strategeens@gmail.com>
2022-01-30 17:28:32 +0700

    Add vertical and horizontal paddings and margins style utility

--- app/styles-utilities.ts
+++ app/styles-utilities.ts
@@ -57,6 +57,56 @@ const utils = StyleSheet.create({
     paddingLeftXS: {paddingLeft: theme.SPACING.XS},
     paddingLeftNone: {paddingLeft: 0},
 
+    paddingVerticalXL: {
+        paddingTop: theme.SPACING.XL,
+        paddingBottom: theme.SPACING.XL
+    },
+    paddingVerticalL: {
+        paddingTop: theme.SPACING.L,
+        paddingBottom: theme.SPACING.L
+    },
+    paddingVerticalM: {
+        paddingTop: theme.SPACING.M,
+        paddingBottom: theme.SPACING.M
+    },
+    paddingVerticalS: {
+        paddingTop: theme.SPACING.S,
+        paddingBottom: theme.SPACING.S
+    },
+    paddingVerticalSM: {
+        paddingTop: theme.SPACING.SM,
+        paddingBottom: theme.SPACING.SM
+    },
+    paddingVerticalXS: {
+        paddingTop: theme.SPACING.XS,
+        paddingBottom: theme.SPACING.XS
+    },
+
+    paddingHorizontalXL: {
+        paddingRight: theme.SPACING.XL,
+        paddingLeft: theme.SPACING.XL
+    },
+    paddingHorizontalL: {
+        paddingRight: theme.SPACING.L,
+        paddingLeft: theme.SPACING.L
+    },
+    paddingHorizontalM: {
+        paddingRight: theme.SPACING.M,
+        paddingLeft: theme.SPACING.M
+    },
+    paddingHorizontalS: {
+        paddingRight: theme.SPACING.S,
+        paddingLeft: theme.SPACING.S
+    },
+    paddingHorizontalSM: {
+        paddingRight: theme.SPACING.SM,
+        paddingLeft: theme.SPACING.SM
+    },
+    paddingHorizontalXS: {
+        paddingRight: theme.SPACING.XS,
+        paddingLeft: theme.SPACING.XS
+    },
+
     marginTopXL: {marginTop: theme.SPACING.XL},
     marginTopL: {marginTop: theme.SPACING.L},
     marginTopM: {marginTop: theme.SPACING.M},
@@ -87,7 +137,57 @@ const utils = StyleSheet.create({
     marginLeftS: {marginLeft: theme.SPACING.S},
     marginLeftSM: {marginLeft: theme.SPACING.SM},
     marginLeftXS: {marginLeft: theme.SPACING.XS},
-    marginLeftNone: {marginLeft: 0}
+    marginLeftNone: {marginLeft: 0},
+
+    marginVerticalXL: {
+        marginTop: theme.SPACING.XL,
+        marginBottom: theme.SPACING.XL
+    },
+    marginVerticalL: {
+        marginTop: theme.SPACING.L,
+        marginBottom: theme.SPACING.L
+    },
+    marginVerticalM: {
+        marginTop: theme.SPACING.M,
+        marginBottom: theme.SPACING.M
+    },
+    marginVerticalS: {
+        marginTop: theme.SPACING.S,
+        marginBottom: theme.SPACING.S
+    },
+    marginVerticalSM: {
+        marginTop: theme.SPACING.SM,
+        marginBottom: theme.SPACING.SM
+    },
+    marginVerticalXS: {
+        marginTop: theme.SPACING.XS,
+        marginBottom: theme.SPACING.XS
+    },
+
+    marginHorizontalXL: {
+        marginRight: theme.SPACING.XL,
+        marginLeft: theme.SPACING.XL
+    },
+    marginHorizontalL: {
+        marginRight: theme.SPACING.L,
+        marginLeft: theme.SPACING.L
+    },
+    marginHorizontalM: {
+        marginRight: theme.SPACING.M,
+        marginLeft: theme.SPACING.M
+    },
+    marginHorizontalS: {
+        marginRight: theme.SPACING.S,
+        marginLeft: theme.SPACING.S
+    },
+    marginHorizontalSM: {
+        marginRight: theme.SPACING.SM,
+        marginLeft: theme.SPACING.SM
+    },
+    marginHorizontalXS: {
+        marginRight: theme.SPACING.XS,
+        marginLeft: theme.SPACING.XS
+    }
 });
 
 export default utils;
