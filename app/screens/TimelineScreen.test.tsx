48d187182ea05ed5404e5f626ed98d2b43ebdcc1
klaudijus <klaudijus.strategeens@gmail.com>
2022-02-03 16:19:32 +0700

    Connect timeline screen to state

---
+++ app/screens/TimelineScreen.test.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {Activity} from '../types/Activity';
+import {Entry} from '../types/Entry';
+import {useEntriesStore} from '../state/entries';
+import TimelineScreen from './TimelineScreen';
+import {useActivityFilterStore} from '../state/activityFilter';
+import {useTimelineSettingsStore} from '../state/timeline-settings';
+import {TimeInterval} from '../types/TimeInterval';
+
+const entries: Entry[] = [
+    {
+        id: '4',
+        activity: Activity.SWIMMING,
+        distance: 2.5,
+        duration: 3000,
+        date: '2022-01-01T09:10:02.207Z',
+        effort: 4
+    },
+    {
+        id: '3',
+        activity: Activity.CYCLING,
+        distance: 50,
+        duration: 7200,
+        date: '2021-09-01T09:10:02.207Z',
+        effort: 5
+    },
+    {
+        id: '2',
+        activity: Activity.SWIMMING,
+        distance: 2,
+        duration: 3600,
+        date: '2021-04-10T09:10:02.207Z',
+        effort: 1
+    },
+    {
+        id: '1',
+        activity: Activity.RUNNING,
+        distance: 10,
+        duration: 3600,
+        date: '2021-04-07T09:10:02.207Z',
+        effort: 3
+    }
+] as Entry[];
+
+const navigation = {};
+
+describe('TimelineScreen', () => {
+    it('should display monthly timeline entries', () => {
+        useTimelineSettingsStore.setState({timeInterval: TimeInterval.MONTH});
+        useEntriesStore.setState({entries});
+        const component = <TimelineScreen navigation={navigation} />;
+        const {getByText} = render(component);
+        getByText('January, 2022');
+        getByText('September, 2021');
+        getByText('April, 2021');
+    });
+
+    it('should display yearly timeline entries', () => {
+        useTimelineSettingsStore.setState({timeInterval: TimeInterval.YEAR});
+        useEntriesStore.setState({entries});
+        const component = <TimelineScreen navigation={navigation} />;
+        const {getByText} = render(component);
+        getByText('2022');
+        getByText('2021');
+    });
+
+    it('should filter timeline entries by activity', () => {
+        useActivityFilterStore.setState({filter: Activity.CYCLING});
+        useEntriesStore.setState({entries});
+        const component = <TimelineScreen navigation={navigation} />;
+        const {queryByText, getByText} = render(component);
+        getByText('September, 2021');
+        expect(queryByText('January, 2022')).toBeNull();
+        expect(queryByText('April, 2021')).toBeNull();
+    });
+});
--- app/screens/TimelineScreen.tsx
+++ app/screens/TimelineScreen.tsx
@@ -1,30 +1,21 @@
-import React, {useLayoutEffect, useState} from 'react';
-import {
-    ScrollView,
-    StyleSheet,
-    TouchableNativeFeedback,
-    View
-} from 'react-native';
+import React from 'react';
+import {ScrollView, View} from 'react-native';
 import appStyles from '../styles';
-import theme from '../theme';
-import HeaderButton from '../components/header/HeaderButton';
 import HeaderActivityFilter from '../components/header/HeaderActivityFilter';
-import TrophiesOverview from '../components/trophy/TrophiesOverview';
-import TrophiesTimeline from '../components/trophy/TrophiesTimeline';
-import {PrimaryHeader} from '../components/ui/Text';
 import {useActivityFilterStore} from '../state/activityFilter';
-import {getTrophies, useTrophiesStore} from '../state/trophies';
-import TrophyCard from '../components/trophy/TrophyCard';
 import TimelineCard from '../components/timeline/TimelineCard';
 import SelectionButtons from '../components/shared/SelectionButtons';
 import utils from '../styles-utilities';
 import {TimeInterval} from '../types/TimeInterval';
+import {TimelineEntry} from '../types/TimelineEntry';
+import {useEntriesStore} from '../state/entries';
+import {getTimelineEntries} from '../state/timelineEntries';
+import {
+    setTimelineSettings,
+    useTimelineSettingsStore
+} from '../state/timeline-settings';
 
 const timeIntervalValues = [
-    // {
-    //     label: 'Week',
-    //     value: TimeInterval.WEEK
-    // },
     {
         label: 'Month',
         value: TimeInterval.MONTH
@@ -33,76 +24,22 @@ const timeIntervalValues = [
         label: 'Year',
         value: TimeInterval.YEAR
     }
-    // {
-    //     label: 'All',
-    //     value: null
-    // }
-];
-
-const data = [
-    {
-        title: 'January, 2022',
-        distance: 100,
-        duration: 36000,
-        effort: 5,
-        workouts: 12
-    },
-    {
-        title: 'December, 2021',
-        distance: 73,
-        duration: 30000,
-        effort: 3,
-        workouts: 10
-    },
-    {
-        title: 'November, 2021',
-        distance: 52,
-        duration: 15000,
-        effort: 4,
-        workouts: 7
-    },
-    {
-        title: 'October, 2021',
-        distance: 15,
-        duration: 5000,
-        effort: 1,
-        workouts: 2
-    },
-    {
-        title: 'September, 2021',
-        distance: 25,
-        duration: 3500,
-        effort: 2,
-        workouts: 3
-    }
 ];
 
 function TimelineScreen({navigation}) {
-    // const {filter} = useActivityFilterStore();
-    // const trophies = useTrophiesStore((state) => getTrophies(state, filter));
+    const {filter} = useActivityFilterStore();
+    const {timeInterval} = useTimelineSettingsStore();
+    const timelineEntries = useEntriesStore(
+        getTimelineEntries(filter, timeInterval)
+    );
 
-    const navigateToDetails = (item) => {
-        navigation.navigate('timeline-details', item);
+    const navigateToDetails = (entry: TimelineEntry) => {
+        navigation.navigate('timeline-details', entry);
     };
 
-    // useLayoutEffect(() => {
-    //     navigation.setOptions({
-    //         headerStyle: {
-    //             height: theme.HEADER_HEIGHT,
-    //             backgroundColor: theme.COLORS.BACKGROUND_PRIMARY,
-    //             elevation: 0
-    //         },
-    //         // headerLeft: null,
-    //         // headerRight: () => (
-    //         //     <HeaderButton
-    //         //         iconName="plus"
-    //         //         onPress={() => navigation.navigate('trophy-form', {})}
-    //         //     />
-    //         // )
-    //     });
-    // }, [navigation]);
-
-    const [timeInterval, setTimeInterval] = useState(timeIntervalValues[0]);
+    const setTimeInterval = (
+        interval: TimeInterval.MONTH | TimeInterval.YEAR
+    ) => setTimelineSettings({timeInterval: interval});
 
     return (
         <>
@@ -115,32 +52,13 @@ function TimelineScreen({navigation}) {
                         style={[utils.marginHorizontalM, utils.marginVerticalM]}
                         onChange={setTimeInterval}
                     />
-                    {data.map((item) => (
+                    {timelineEntries.map((entry) => (
                         <TimelineCard
-                            key={item.title}
-                            title={item.title}
-                            duration={item.duration}
-                            distance={item.distance}
-                            effort={item.effort}
-                            workouts={item.workouts}
-                            onPress={() => {
-                                console.log('on press');
-                                navigateToDetails(item);
-                            }}
+                            key={entry.title}
+                            timelineEntry={entry}
+                            onPress={() => navigateToDetails(entry)}
                         />
                     ))}
-                    {/* <TrophiesOverview /> */}
-                    {/* <TrophiesTimeline onPress={navigateToTrophyDetails} /> */}
-                    {/* <ScrollView> */}
-                    {/*     {trophies.map((trophy) => ( */}
-                    {/*         <TrophyCard */}
-                    {/*             key={trophy.id} */}
-                    {/*             trophy={trophy} */}
-                    {/*             onPress={() => {}} */}
-                    {/*         /> */}
-                    {/*     ))} */}
-                    {/* </ScrollView> */}
-                    {/* <PrimaryHeader>SWIMMING</PrimaryHeader> */}
                 </ScrollView>
             </View>
         </>
