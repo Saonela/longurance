3edcbd905833586326e6a376278f26cf34d72101
klaudijus <klaudijus.strategeens@gmail.com>
2022-02-03 17:43:39 +0700

    Connect timeline details screen with state

--- app/components/timeline/TimelineDetails.tsx
+++ app/components/timeline/TimelineDetails.tsx
@@ -4,6 +4,8 @@ import {SecondaryHeader, SecondaryText} from '../ui/Text';
 import Panel from '../ui/Panel';
 import theme from '../../theme';
 import {
+    calculatePace,
+    getCalculatedPaceText,
     getDistanceText,
     getDurationText,
     getIntensityText,
@@ -14,83 +16,105 @@ import {
 import utils from '../../styles-utilities';
 import Separator from '../ui/Separator';
 import LineChart from '../chart/LineChart';
+import {TimelineEntry} from '../../types/TimelineEntry';
+import {getEntriesByIds, useEntriesStore} from '../../state/entries';
 
-interface TimelineDetailsProps {}
+interface TimelineDetailsProps {
+    timelineEntry: TimelineEntry;
+}
 
-function TimelineDetails({item}) {
+function TimelineDetails({timelineEntry}: TimelineDetailsProps) {
+    const entries = useEntriesStore(getEntriesByIds(timelineEntry.entryIds));
+    const distancePoints = entries.map((entry) => entry.distance);
+    const durationPoints = entries.map((entry) => entry.duration);
+    const intensityPoints = entries.map((entry) => entry.effort);
+    const pacePoints = entries.map((entry) =>
+        calculatePace(entry.duration, entry.distance)
+    );
     return (
         <Panel>
             <SecondaryHeader color="secondary" style={[utils.marginBottomXL]}>
-                {item.title}
+                {timelineEntry.title}
             </SecondaryHeader>
             <View style={utils.row}>
                 <View style={utils.flex1}>
                     <SecondaryHeader style={styles.textHeader}>
-                        {item.workouts}
+                        {timelineEntry.entryIds.length}
                     </SecondaryHeader>
                     <SecondaryText>
-                        {getWorkoutsLabel(item.workouts)}
+                        {getWorkoutsLabel(timelineEntry.entryIds.length)}
                     </SecondaryText>
                 </View>
                 <View style={utils.flex1}>
                     <SecondaryHeader style={styles.textHeader}>
-                        {item.workouts}
+                        {timelineEntry.trophiesCount}
                     </SecondaryHeader>
                     <SecondaryText>
-                        {getTrophiesLabel(item.workouts)}
+                        {getTrophiesLabel(timelineEntry.trophiesCount)}
                     </SecondaryText>
                 </View>
             </View>
-            <Separator
-            // marginTop={theme.SPACING.L}
-            // marginBottom={theme.SPACING.L}
-            />
+            <Separator marginBottom={theme.SPACING.L} />
             <View>
                 <SecondaryHeader style={styles.textHeader}>
-                    {getDistanceText(item.distance)}
+                    {getDistanceText(timelineEntry.distance)}
                 </SecondaryHeader>
                 <SecondaryText>Distance</SecondaryText>
             </View>
             <LineChart
                 style={styles.chart}
-                data={[
-                    50, 10, 40, 95, 4, 24, 85, 91, 35, 53, 53, 24, 50, 20, 80
-                ]}
+                data={distancePoints}
                 formatLabel={getDistanceText}
             />
             <Separator
-            // marginTop={theme.SPACING.L}
-            // marginBottom={theme.SPACING.L}
+                marginTop={theme.SPACING.L}
+                marginBottom={theme.SPACING.L}
             />
             <View>
                 <SecondaryHeader style={styles.textHeader}>
-                    {getDurationText(item.duration)}
+                    {getDurationText(timelineEntry.duration)}
                 </SecondaryHeader>
                 <SecondaryText>Duration</SecondaryText>
             </View>
-            <LineChart style={styles.chart} data={[]} />
+            <LineChart
+                style={styles.chart}
+                data={durationPoints}
+                formatLabel={getDurationText}
+            />
             <Separator
-            // marginTop={theme.SPACING.L}
-            // marginBottom={theme.SPACING.L}
+                marginTop={theme.SPACING.L}
+                marginBottom={theme.SPACING.L}
             />
             <View>
                 <SecondaryHeader style={styles.textHeader}>
-                    {getPaceText(item.duration, item.distance)}
+                    {getPaceText(
+                        timelineEntry.duration,
+                        timelineEntry.distance
+                    )}
                 </SecondaryHeader>
                 <SecondaryText>Avg. Pace</SecondaryText>
             </View>
-            <LineChart style={styles.chart} data={[]} />
+            <LineChart
+                style={styles.chart}
+                data={pacePoints}
+                formatLabel={getCalculatedPaceText}
+            />
             <Separator
-            // marginTop={theme.SPACING.L}
-            // marginBottom={theme.SPACING.L}
+                marginTop={theme.SPACING.L}
+                marginBottom={theme.SPACING.L}
             />
             <View>
                 <SecondaryHeader style={styles.textHeader}>
-                    {getIntensityText(item.effort)}
+                    {getIntensityText(timelineEntry.effort)}
                 </SecondaryHeader>
                 <SecondaryText>Avg. Intensity</SecondaryText>
             </View>
-            <LineChart style={utils.marginTopL} data={[]} />
+            <LineChart
+                style={styles.chart}
+                data={intensityPoints}
+                yAxisData={[1, 2, 3, 4, 5]}
+                formatLabel={getIntensityText}
+            />
         </Panel>
     );
 }
--- app/screens/TimelineDetailsScreen.tsx
+++ app/screens/TimelineDetailsScreen.tsx
@@ -1,36 +1,16 @@
-import React, {useLayoutEffect} from 'react';
-import {Alert, ScrollView, View} from 'react-native';
-import HeaderButton from '../components/header/HeaderButton';
+import React from 'react';
+import {ScrollView, View} from 'react-native';
 import appStyles from '../styles';
-import EntryDetails from '../components/entry/EntryDetails';
-import {deleteEntry, getEntry, useEntriesStore} from '../state/entries';
-import {Entry} from '../types/Entry';
-import {
-    getEntryIndividualTrophies,
-    updateCompletedTrophies,
-    useTrophiesStore
-} from '../state/trophies';
-import theme from '../theme';
-import TrophyCardLite from '../components/trophy/TrophyCardLite';
 import TimelineDetails from '../components/timeline/TimelineDetails';
+import {TimelineEntry} from '../types/TimelineEntry';
 
-function TimelineDetailsScreen({route, navigation}) {
-    console.log('TimelineDetailsScreen', route.params);
-    const item = route.params;
-
+function TimelineDetailsScreen({route}) {
+    const timelineEntry: TimelineEntry = route.params;
     return (
         <View style={appStyles.screenContainer}>
             <ScrollView>
-                <TimelineDetails item={item} />
+                <TimelineDetails timelineEntry={timelineEntry} />
             </ScrollView>
-            {/* <ScrollView */}
-            {/*     contentContainerStyle={{paddingBottom: theme.SPACING.M}} */}
-            {/* > */}
-            {/*     {entry && <EntryDetails entry={entry} />} */}
-            {/*     {trophies.map((trophy) => ( */}
-            {/*         <TrophyCardLite key={trophy.id} trophy={trophy} /> */}
-            {/*     ))} */}
-            {/* </ScrollView> */}
         </View>
     );
 }
