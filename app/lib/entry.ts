9b3be3d76b31be71825085be820d3e0af2eaf96a
klaudijus <klaudijus.strategeens@gmail.com>
2022-02-11 18:51:03 +0700

    Fix entry.ts lib ESLint error

--- app/lib/entry.ts
+++ app/lib/entry.ts
@@ -47,6 +47,7 @@ export function getActivityTypeText(type: Activity) {
     if (type === Activity.CYCLING) return 'CYCLE';
     if (type === Activity.SWIMMING) return 'SWIM';
     if (type === Activity.RUNNING) return 'RUN';
+    return null;
 }
 
 export function getEntriesFieldValues(entries: Entry[]) {
